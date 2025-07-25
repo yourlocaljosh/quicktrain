9f51e2b1b9f1e9cb292bdf68fbd7fd2469692083
Josh <josh.seungwon.hong@gmail.com>
2025-07-25 15:04:56 -0400

    Add react structure

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [currentStep, setCurrentStep] = useState(1);
+  
   return (
     <div>
-      <h1>QuickTrain</h1>
-      <p>UNDER CONSTRUCTION</p>
+      <header>
+        <h1>quicktrain</h1>
+      </header>
+      <main>
+        {currentStep === 1 && <div>Step 1 Content</div>}
+        {currentStep === 2 && <div>Step 2 Content</div>}
+        {currentStep === 3 && <div>Step 3 Content</div>}
+      </main>
     </div>
   );
 };
