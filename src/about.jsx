2677c2a4624ae5c0c64f98f232c865f98d0a7321
joshsw <81436003+yourlocaljosh@users.noreply.github.com>
2026-01-03 23:26:46 -0500

    Add note about artist in the about section

--- src/about.jsx
+++ src/about.jsx
@@ -12,6 +12,7 @@ const About = () => {
         <div className="bg-white rounded-2xl shadow-lg p-8">
           <h1 className="text-3xl font-bold text-gray-900 mb-6 text-center">About</h1>
           <p className="text-gray-600 text-center">Thank you for using quicktrain!</p>
+          <p className="text-gray-300 text-center">Side note: I (was) looking for an artist to create muscle and exercise sprites for the Encyclopedia</p>
           <p className="text-blue-600 text-center"><a href="https://www.joshsw.me">Created by "me".</a></p>
         </div>
       </main>
